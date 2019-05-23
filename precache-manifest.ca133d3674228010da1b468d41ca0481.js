721fa9ca9ff1b4ad7bd7f71d44ecda303bf19c13
addtheletters <addtheletters@users.noreply.github.com>
2019-05-23 11:50:52 -0400

    Updates

---
+++ precache-manifest.ca133d3674228010da1b468d41ca0481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba7d79ff4eadbd413cbf7c8ea3c03d8",
+    "url": "/token-clipper/index.html"
+  },
+  {
+    "revision": "de58b158cfede0ed7c6f",
+    "url": "/token-clipper/static/css/main.fdcf4de8.chunk.css"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/token-clipper/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "de58b158cfede0ed7c6f",
+    "url": "/token-clipper/static/js/main.1316e119.chunk.js"
+  },
+  {
+    "revision": "1b1b62d88d1898130d60",
+    "url": "/token-clipper/static/js/runtime~main.1aafd3b3.js"
+  }
+]);
